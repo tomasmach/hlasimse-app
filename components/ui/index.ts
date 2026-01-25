7939fd26d96628f285646f5749f4b368c2986579
tomasmach <tomades1@gmail.com>
2026-01-25 23:37:44 +0100

    feat: add UI components barrel export

---
+++ components/ui/index.ts
@@ -0,0 +1,6 @@
+// components/ui/index.ts
+export { GradientButton } from "./GradientButton";
+export { AnimatedInput } from "./AnimatedInput";
+export { GradientAvatar } from "./GradientAvatar";
+export { Card } from "./Card";
+export { Toast } from "./Toast";
